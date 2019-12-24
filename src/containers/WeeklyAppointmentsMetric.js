0e081477dffb0bd3c58bf29c5487f6fb8f19da0b
Altaf Quadri <altafmquadri@gmail.com>
2019-12-24 18:07:03 -0500

    changed chart components to functions rather than classes

--- src/containers/WeeklyAppointmentsMetric.js
+++ src/containers/WeeklyAppointmentsMetric.js
@@ -1,108 +1,13 @@
-import React, { Component } from 'react';
-import moment from 'moment'
+import React from 'react';
 import { Line } from 'react-chartjs-2'
 
-class WeeklyAppointmentsMetric extends Component {
-    // state = { 
-        // currentUser: this.props.currentUser,
-        // currentDate: moment(),
-        // appointmentDates: [],
-        // filteredAppointmentDatesByWeek: [],
-        // daysOfWeek: [],
-        // countOfApptInDaysOfWeek: [],
-        // lineChartData: {}
-    // }
+const WeeklyAppointmentsMetric =  (props) => {
 
-    // componentDidMount() {
-        // let newDates, filteredAppointmentDatesByWeek
-        // //take all appointments
-        // newDates = this.props.appointments.map(appointment => appointment.date)
-        // //take appointments and filter them by current week
-        // //compare today's date with the array of appointments
-        // filteredAppointmentDatesByWeek = newDates.filter(date => moment(date).isSame(this.state.currentDate, 'week'))
-        // this.setState(
-        //     { 
-        //         appointmentDates: [...this.state.appointmentDates, ...newDates],
-        //         filteredAppointmentDatesByWeek: [...this.state.filteredAppointmentDatesByWeek, ...filteredAppointmentDatesByWeek],
-        //         daysOfWeek: [...this.state.daysOfWeek, ...this.getDaysOfTheWeek()],
-        //     }, () => this.setState(
-        //         {
-        //             countOfApptInDaysOfWeek: [...this.state.countOfApptInDaysOfWeek, ...this.countOfApptInDaysOfWeek(this.state.daysOfWeek, this.state.filteredAppointmentDatesByWeek)]  
-        //         }, () => this.setState({ lineChartData: {...this.setLineChartData()}  })))
-    // }
-
-    // getDaysOfTheWeek = () => {
-    //     let beginningOfWeek = moment().startOf('week')
-    //     let daysOfWeek = []
-    //     //used knowledge of today's date and the start of the week to iterate days to the end of the week
-    //     daysOfWeek.push(beginningOfWeek.format('YYYY-MM-DD'))
-    //     for (let i = 1; i < 7; i++) {
-    //         daysOfWeek.push(beginningOfWeek.add(1, 'days').format('YYYY-MM-DD'))
-    //     }
-    //     return daysOfWeek
-    // }
-
-    // // console.log(moment().isSame(moment('2019-12-23'),'date'))
-    // countOfApptInDaysOfWeek = (a, b) => {
-    //     let counts = []
-    //     for (let i = 0; i < a.length; i++) {
-    //         let count = 0;
-    //         for (let j = 0; j < b.length; j++) {
-    //             if (moment(b[j]).isSame(moment(a[i]))) {
-    //                 count++
-    //             }
-    //         }
-    //         counts.push(count)
-    //     }
-    //     return counts
-    // }
-
-    // setLineChartData = () => {
-    //     const chartData = {
-    //         labels: this.state.daysOfWeek,
-    //         datasets: [
-    //             {
-    //                 label: `${this.state.filteredAppointmentDatesByWeek.length} Appointments for the week`,
-    //                 data: this.state.countOfApptInDaysOfWeek,
-    //                 backgroundColor: [
-    //                     'rgba(255, 99, 132, 0.2)',
-    //                     'rgba(54, 162, 235, 0.2)',
-    //                     'rgba(255, 206, 86, 0.2)',
-    //                     'rgba(75, 192, 192, 0.2)',
-    //                     'rgba(153, 102, 255, 0.2)'
-    //                 ],
-    //                 borderColor: [
-    //                     'rgba(255, 99, 132, 1)',
-    //                     'rgba(54, 162, 235, 1)',
-    //                     'rgba(255, 206, 86, 1)',
-    //                     'rgba(75, 192, 192, 1)',
-    //                     'rgba(153, 102, 255, 1)'
-    //                 ],
-    //                 borderWidth: 1
-    //             }]
-    //     }
-    //     return chartData
-    // }
-
-
-    render() {
-        
-        // console.log('i am the state ',this.state)
-        // console.log('i am the daysOfWeek ',this.state.daysOfWeek[0])
-        // console.log(this.state.currentDate.toDate())
-        
-        // let beginningOfWeek = moment().startOf('week').add(6, 'days').toDate()
-        // let endOfWeek = moment().endOf('week').date()
-        // console.log('i am the days of the week ',this.getDaysOfTheWeek())
-        // console.log(this.countOfApptInDaysOfWeek(this.state.daysOfWeek, this.state.filteredAppointmentDatesByWeek))
-
-       
-       
         return ( 
                 <div className="weekly-appointments-metric">
                     <h2>Weekly Appointments</h2>
                     <Line 
-                        data={this.props.lineChartData}
+                        data={props.lineChartData}
                         options={{
                             scales: {
                                 yAxes: [{
@@ -118,6 +23,5 @@ class WeeklyAppointmentsMetric extends Component {
                 </div>
         );
     }
-}
 
 export default WeeklyAppointmentsMetric;
--- src/containers/WeeklyProduction.js
+++ src/containers/WeeklyProduction.js
@@ -2,84 +2,14 @@ import React from 'react';
 import moment from 'moment'
 import { Polar } from 'react-chartjs-2'
 
-class WeeklyProduction extends React.Component {
+const WeeklyProduction = (props) => {
 
-    // state = { 
-        // currentDate: moment(),
-        // currentUser: this.props.currentUser,
-        // appointments: this.props.appointments,
-        // weekAppointments: [],
-        // presentations: [],
-        // weeklySales: [],
-        // polarChartData: {}
-    // }
-
-    // setPolarChartData = () => {
-    //     const polarChartData = {
-    //         labels: ['Appointments', 'Presentations', 'Sales'],
-    //         datasets: [
-    //             {
-    //                 label: `$Weekly Presentation`,
-    //                 data: [this.state.weekAppointments.length, this.state.presentations.length, this.state.weeklySales.length],
-    //                 backgroundColor: [
-    //                     'rgba(153, 102, 255, 0.2)',
-    //                     'rgba(255, 99, 132, 0.2)',
-    //                     'rgba(54, 162, 235, 0.2)',
-    //                     'rgba(255, 206, 86, 0.2)',
-    //                     'rgba(75, 192, 192, 0.2)'
-    //                 ],
-    //                 borderColor: [
-    //                     'rgba(255, 99, 132, 1)',
-    //                     'rgba(54, 162, 235, 1)',
-    //                     'rgba(255, 206, 86, 1)',
-    //                     'rgba(75, 192, 192, 1)',
-    //                     'rgba(153, 102, 255, 1)'
-    //                 ],
-    //                 borderWidth: 1
-    //             }]
-    //     }
-    //     return polarChartData
-    // }
-
-    
-
-    // componentDidUpdate(prevState){
-    //     if (prevState.currentUser !== this.props.currentUser) {
-    //         this.setState(
-    //             { 
-    //                 currentUser: this.props.currentUser,
-    //                 appointments: this.props.appointments,
-                     
-    //         }, () => this.setState({ presentations: this.getWeeklyPresentations() }))
-    //     }
-    // } 
-    
-    // getWeeklyPresentations = () => {
-    //     let filteredAppointmentsByWeek = this.state.appointments.filter(appointment => moment(appointment.date).isSame(this.state.currentDate, 'week'))
-    //     let salesonWeeklyAppointments = filteredAppointmentsByWeek.filter(appointment => appointment['made_sale?'])
-    //     this.setState(
-    //         { 
-    //             weekAppointments: filteredAppointmentsByWeek,
-    //             weeklySales: salesonWeeklyAppointments  
-    //         }, () => this.setState({ polarChartData: this.setPolarChartData()  }));
-    //     let presentationAppointments = filteredAppointmentsByWeek.filter(appointment => appointment['presentation_made?'])
-    //     return presentationAppointments
-    // }
-
-
-    render() { 
-        // console.log('i am props', this.props)
-        // console.log('i am state',this.state)
-        // console.log('filterAppp' , this.getWeeklyPresentations())
         return ( 
             <div className="weekly-presentation-metric">
                 <h2>Weekly Production</h2>
-                <Polar 
-                        data={this.props.polarChartData}
-                    />
+                <Polar data={props.polarChartData}/>
             </div>
         );
     }
-}
 
 export default WeeklyProduction;
